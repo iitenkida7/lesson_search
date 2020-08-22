3b5e9765904c2fe0defd7dba83f57c22237a602a
iitenkida7 <i.takeshi2222@gmail.com>
2020-08-22 17:11:44 +0900

    見た目の改善

--- myapp/view/index.php
+++ myapp/view/index.php
@@ -1,20 +1,38 @@
-<form action="index.php" method="post">
-  <h2>登録</h2>
-  <input type="text" name="words" size="100" maxlength="100">
-  <input type="submit" value="送信">
-</form>
+<!DOCTYPE html>
+<html lang="ja">
 
-<form action="index.php" method="post">
-  <h2>検索</h2>
-  <input type="text" name="keyword"
-    value="<?php echo $_POST['keyword']?>"
-    size="100" maxlength="100">
-  <input type="submit" value="送信">
-</form>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+  <title>Search</title>
+</head>
 
+<body>
+  <main>
+    <h1>Search</h1>
 
-<?php   if ($searchResults) { ?>
-<h3>結果</h3>
-<?php
-    echo implode(" ", $searchResults);
+    <form action="/" method="post">
+      <h2>登録</h2>
+      <p>スペース区切りで単語を入力</p>
+      <input type="text" name="words" size="50" maxlength="1000">
+      <input type="submit" value="登録">
+    </form>
+
+    <form action="index.php" method="post">
+      <h2>検索</h2>
+      <input type="text" name="keyword"
+        value="<?php echo $_POST['keyword']?>"
+        size="50" maxlength="20">
+      <input type="submit" value="検索">
+    </form>
+
+    <?php   if ($searchResults) { ?>
+    <h3>結果</h3>
+    <pre>
+  <?php
+      echo implode(" ", $searchResults);
   }
+?>
+  </pre>
+  </main>
+</body>
